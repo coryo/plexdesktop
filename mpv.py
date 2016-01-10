59c668ae590649609b463320396a28099b8900bf
Cory Parsons <babylonstudio@gmail.com>
2016-01-10 16:48:19 -0500

    fix missing class name

--- mpv.py
+++ mpv.py
@@ -457,11 +457,11 @@ class MPV:
 
     @property
     def video_params(self):
-        return self._get_dict('video-params/', _VIDEO_PARAMS_LIST)
+        return self._get_dict('video-params/', MPV._VIDEO_PARAMS_LIST)
 
     @property
     def video_out_params(self):
-        return self._get_dict('video-out-params/', _VIDEO_PARAMS_LIST)
+        return self._get_dict('video-out-params/', MPV._VIDEO_PARAMS_LIST)
 
     @property
     def playlist(self):
