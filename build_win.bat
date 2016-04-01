f196b60e23c5a12f78e72665984bbb139a5eb88a
Cory Parsons <babylonstudio@gmail.com>
2016-03-31 22:04:45 -0400

    app icon for pyqtdeploy windows build

--- build_win.bat
+++ build_win.bat
@@ -1,3 +1,4 @@
+SET OLDPATH=%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86
 rmdir /S /Q build
 pyqtdeploycli --project plexdesktop-win.pdy build
@@ -12,3 +13,4 @@ cd ..
 mkdir build\release\resources
 xcopy resources build\release\resources
 xcopy mpv-1.dll build\release
+SET PATH=%OLDPATH%
